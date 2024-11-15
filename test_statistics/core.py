04c1c9c97d15fc8ca038626d309252fb7128d7f7
Oliver Walters <oliver.henry.walters@gmail.com>
2024-11-15 01:55:48 +0000

    Initial project files

---
+++ test_statistics/core.py
@@ -0,0 +1,42 @@
+"""Test statistics plugin for InvenTree."""
+
+from plugin import InventreePlugin
+from plugin.mixins import SettingsMixin, UrlsMixin, UserInterfaceMixin
+
+from .version import PLUGIN_VERSION
+
+
+class TestStatisticsPlugin(SettingsMixin, UrlsMixin, UserInterfaceMixin, InventreePlugin):
+    """Test statistics plugin for InvenTree."""
+
+    AUTHOR = "InvenTree Contributors"
+    DESCRIPTION = "Test statistics plugin for InvenTree"
+    VERSION = PLUGIN_VERSION
+
+    MIN_VERSION = '0.17.0'
+
+    NAME = "Test Statistics"
+    SLUG = "test_statistics"
+    TITLE = "Test Statistics Plugin"
+
+    SETTINGS = {}
+
+    def setup_urls(self):
+        """Returns the URLs defined by this plugin."""
+
+        # TODO: Define the URL patterns for this plugin
+        return []
+
+    def get_ui_panels(self, request, context=None, **kwargs):
+        """Return the UI panels for this plugin."""
+
+        user = request.user
+
+        if not user or not user.is_authenticated:
+            return []
+        
+        # Cache the settings for this plugin
+        self.plugin_settings = self.get_settings_dict()
+
+        # TODO: Define the UI panels for this plugin
+        return []
